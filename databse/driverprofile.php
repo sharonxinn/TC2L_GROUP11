50c1221a9a87a5c90ae1c4a28d3f9666f7d63261
Yiap <wshz2102@gmail.com>
2024-08-20 00:20:38 +0800

    update

--- databse/driverprofile.php
+++ databse/driverprofile.php
@@ -11,18 +11,17 @@
     $qrcode = $_POST['qrcode'];
     $message = $_POST['message'];
 
-	// Database connection
 	$conn = new mysqli('localhost','root','','IDENTITY');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into driverprofile(fullName, gender, dateandTime, pickup, dropoff, carplate, carmodel, totalperson, fees, qrcode, message ) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssssssisis", $fullName, $gender, $dateandTime, $pickup, $dropoff, $carplate, $carmodel, $totalperson, $fees, $qrcode ,$message);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successfully...";
-		$stmt->close();
-		$conn->close();
-	}
+		die("Connection Failed : ". $conn->connect_error);} 
+		else {mysqli_close($conn);
+				$stmt = $conn->prepare("insert into driverprofile(fullName, gender, dateandTime, pickup, dropoff, carplate, carmodel, totalperson, fees, qrcode, message ) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+				$stmt->bind_param("sssssssisis", $fullName, $gender, $dateandTime, $pickup, $dropoff, $carplate, $carmodel, $totalperson, $fees, $qrcode ,$message);
+				$execval = $stmt->execute();
+				echo $execval;
+				echo "Registration successfully...";
+				$stmt->close();
+				$conn->close();
+			}
 ?>

31d195c8ab14e5191582113f1f4c4c4b1d744939
Shi Xiang <shixiang5968@gmail.com>
2024-08-21 01:40:59 +0800

    j

--- website/auth.py
+++ website/auth.py
@@ -15,9 +15,8 @@ def login():
         user=User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully',category='success')
                 login_user(user,remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('auth.driverorpassenger'))
             else:
                 flash('Incorrect password,try again',category='error')
         else:
@@ -29,14 +28,21 @@ def login():
 @login_required
 def profile():
     return render_template('profile.html', user=current_user)
+
 @auth.route('/bookinghisto')
 @login_required
 def bookinghisto():
     return render_template('bookinghisto.html', user=current_user)
+
 @auth.route('/driverprofile')
 @login_required
 def driverprofile():
     return render_template('driverprofile.html', user=current_user)
+
+@auth.route('/driverorpassenger')
+@login_required
+def driverorpassenger():
+    return render_template('driverorpassenger.html', user=current_user)
 
 @auth.route('/passengerprofile')
 @login_required
@@ -85,8 +91,8 @@ def sign_up():
 
             if user:  # Check if user is found
                 login_user(user, remember=True)
-                flash('Account created successfully!', category='success')
-                return redirect(url_for('views.home'))
+                #flash('Account created successfully!', category='success')
+                return redirect(url_for('auth.driverorpassenger'))
             else:
                 flash('Error during user creation. Please try again.', category='error')
 
@@ -97,3 +103,7 @@ def sign_up():
 @auth.route('/about')
 def about():
     return render_template('about.html', user=current_user)
+
+@auth.route('/home')
+def home():
+    return render_template('home.html', user=current_user)

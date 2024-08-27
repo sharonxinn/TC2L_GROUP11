cdfb6f142b37510d86c0404edae68e72bd4f7710
Mui Rui Xin <ruixinmui78@gmail.com>
2024-08-27 21:09:57 +0800

    up

---
+++ TC2L_GROUP11/web/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carpooling.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['MAX_CONTENT_LENGTH']=16*1024*1024
+    app.config['UPLOAD_FOLDER'] = 'web/static/uploads'
+
+
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    from .models import User, Driverspost, Profile, PassengerMatch
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    with app.app_context():
+        db.create_all()
+
+    from .routes import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
